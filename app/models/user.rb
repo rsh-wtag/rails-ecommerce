29f93cba7ccf50409a625bdf16fe64d7249110b2
Radoan Sharkar <radoan.sharkar@welldev.io>
2024-07-26 12:07:53 +0600

    Added User Model and Migration.

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  enum role: { admin: 0, user: 1 }
+
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+  validates :address, presence: true
+  validates :phone, presence: true, phony_plausible: true
+
+  before_save :normalize_phone_number
+
+  private
+
+  def normalize_phone_number
+    self.phone = PhonyRails.normalize_number(phone, default_country_code: 'BD')
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_25_081652) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_26_050536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "address"
+    t.string "phone"
+    t.integer "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
