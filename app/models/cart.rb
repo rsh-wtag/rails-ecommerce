23200b760b7d67c6b145208046ad4d6f450401c5
Radoan Sharkar <radoan.sharkar@welldev.io>
2024-07-29 14:27:05 +0600

    Added Cart and CartItem Model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,6 @@
 class Cart < ApplicationRecord
   belongs_to :user
-  has_many :cart_items
+  has_many :cart_items, dependent: :destroy
 
   validates :user_id, presence: true
   validates :item_count, presence: true, numericality: { greater_than_or_equal_to: 0 }
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,7 +2,7 @@ class CartItem < ApplicationRecord
   belongs_to :cart, dependent: :destroy
   belongs_to :product
 
-  validates :cart_item, presence: true
-  validates :product_id, presense: true, uniqueness: true
+  validates :cart_id, presence: true
+  validates :product_id, presence: true, uniqueness: true
   validates :quantity, numericality: { greater_than_or_equal_to: 1 }
 end
