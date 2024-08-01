6b855946c314dd61de360f21334054c9bd766b7d
Radoan Sharkar <radoan.sharkar@welldev.io>
2024-08-01 11:33:09 +0600

    Updated User model and migration to configure devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,10 @@ class User < ApplicationRecord
 
   enum role: { admin: 0, user: 1 }
 
-  has_secure_password
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
   validates :address, presence: true
   validates :phone, presence: true, phony_plausible: true
 
--- db/migrate/20240726050536_create_users.rb
+++ db/migrate/20240726050536_create_users.rb
@@ -2,13 +2,20 @@ class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
-      t.string :password_digest
+      t.string :email, null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
       t.string :address
       t.string :phone
-      t.integer :role
+      t.integer :role, default: 1
+
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+      t.datetime :remember_created_at
 
       t.timestamps
     end
+
+    add_index :users, :email, unique: true
+    add_index :users, :reset_password_token, unique: true
   end
 end
