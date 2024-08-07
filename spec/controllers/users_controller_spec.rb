9fcd6fc8b756ad5e559473fe8664985a91494668
Radoan Sharkar <radoan.sharkar@welldev.io>
2024-08-07 11:58:22 +0600

    Updated User Controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,96 +1,96 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  let(:valid_attributes) { attributes_for(:user) }
-  let(:invalid_attributes) { { name: '', email: 'invalid', password: 'short' } }
   let(:user) { create(:user) }
 
   describe 'GET #index' do
-    it 'should return a success response' do
-      user
-      get :index
-      response.should be_successful
-    end
+    before { get :index }
+
+    it { is_expected.to respond_with(:success) }
+    it { expect(assigns(:users)).to eq([user]) }
   end
 
   describe 'GET #show' do
-    it 'should return a success response' do
-      get :show, params: { id: user.to_param }
-      response.should be_successful
-    end
+    before { get :show, params: { id: user.id } }
+
+    it { is_expected.to respond_with(:success) }
+    it { expect(assigns(:user)).to eq(user) }
   end
 
   describe 'GET #new' do
-    it 'should return a success response' do
-      get :new
-      response.should be_successful
-    end
+    before { get :new }
+
+    it { is_expected.to respond_with(:success) }
+    it { expect(assigns(:user)).to be_a_new(User) }
   end
 
   describe 'GET #edit' do
-    it 'should return a success response' do
-      get :edit, params: { id: user.to_param }
-      response.should be_successful
-    end
+    before { get :edit, params: { id: user.id } }
+
+    it { is_expected.to respond_with(:success) }
+    it { expect(assigns(:user)).to eq(user) }
   end
 
   describe 'POST #create' do
-    context 'with valid params' do
-      it 'should create a new User' do
+    let(:valid_attributes) do
+      { name: 'Test User', email: 'test@example.com', password: 'password', address: '123 Test St',
+        phone: '+8801234567890' }
+    end
+
+    let(:invalid_attributes) do
+      { name: '', email: '', password: '', address: '', phone: '' }
+    end
+
+    context 'with valid parameters' do
+      it 'creates a new User and redirects to the created user' do
         expect do
           post :create, params: { user: valid_attributes }
         end.to change(User, :count).by(1)
-      end
 
-      it 'should redirect to the created user' do
-        post :create, params: { user: valid_attributes }
-        response.should redirect_to(User.last)
+        created_user = User.last
+        expect(response).to redirect_to(user_path(created_user))
       end
     end
 
-    context 'with invalid params' do
-      it "should return a success response (i.e., to display the 'new' template)" do
-        post :create, params: { user: invalid_attributes }
-        response.should be_successful
-      end
+    context 'with invalid parameters' do
+      before { post :create, params: { user: invalid_attributes } }
+
+      it { is_expected.to render_template(:new) }
     end
   end
 
-  describe 'PUT #update' do
-    context 'with valid params' do
-      let(:new_attributes) { { name: 'Jane Doe' } }
-
-      it 'should update the requested user' do
-        put :update, params: { id: user.to_param, user: new_attributes }
-        user.reload
-        user.name.should eq('Jane Doe')
-      end
-
-      it 'should redirect to the user' do
-        put :update, params: { id: user.to_param, user: valid_attributes }
-        response.should redirect_to(user)
-      end
+  describe 'PATCH #update' do
+    let(:new_attributes) do
+      { name: 'Updated User' }
     end
 
-    context 'with invalid params' do
-      it "should return a success response (i.e., to display the 'edit' template)" do
-        put :update, params: { id: user.to_param, user: invalid_attributes }
-        response.should be_successful
-      end
+    let(:invalid_attributes) do
+      { name: '' }
+    end
+
+    context 'with valid parameters' do
+      before { patch :update, params: { id: user.id, user: new_attributes } }
+
+      it { is_expected.to redirect_to(user) }
+      it { expect(user.reload.name).to eq('Updated User') }
+    end
+
+    context 'with invalid parameters' do
+      before { patch :update, params: { id: user.id, user: invalid_attributes } }
+
+      it { is_expected.to render_template(:edit) }
     end
   end
 
   describe 'DELETE #destroy' do
-    it 'should destroy the requested user' do
-      user
-      expect do
-        delete :destroy, params: { id: user.to_param }
-      end.to change(User, :count).by(-1)
-    end
+    let!(:user_to_destroy) { create(:user) }
 
-    it 'should redirect to the users list' do
-      delete :destroy, params: { id: user.to_param }
-      response.should redirect_to(users_url)
+    it 'destroys the requested user and redirects to users_url' do
+      expect do
+        delete :destroy, params: { id: user_to_destroy.id }
+      end.to change(User, :count).by(-1)
+
+      expect(response).to redirect_to(users_url)
     end
   end
 end
