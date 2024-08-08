973b5b43e60fde52e4a07199086b185e68bf47d9
Radoan Sharkar <radoan.sharkar@welldev.io>
2024-08-08 14:06:51 +0600

    Added Order and OrderItem Controllers and Views

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :reviews, only: %i[edit update destroy]
   resources :categories
   resources :users
+  resources :orders do
+    resources :order_items
+  end
 
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
