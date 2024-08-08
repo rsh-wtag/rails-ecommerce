bc0edd9ded62e585e3a90dcf2be1da6b208a5185
Radoan Sharkar <radoan.sharkar@welldev.io>
2024-08-08 14:57:01 +0600

    Added Payment Controller and Views

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,17 @@ Rails.application.routes.draw do
   resources :products do
     resources :reviews
   end
+
   resources :reviews, only: %i[edit update destroy]
   resources :categories
   resources :users
+
   resources :orders do
     resources :order_items
+    resources :payments, only: %i[new create edit update destroy]
   end
+
+  resources :payments, only: %i[index show]
 
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
