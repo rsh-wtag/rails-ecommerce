385accf3a02796874339f613890dccf3f7e52c28
Radoan Sharkar <radoan.sharkar@welldev.io>
2024-08-25 09:28:02 +0600

    Update CartItem Controller.

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -9,34 +9,38 @@ class CartItemsController < ApplicationController
     if current_user
       @cart = current_user.cart
       existing_cart_item = @cart.cart_items.find_by(product_id:)
-
-      if existing_cart_item
-        existing_cart_item.update(quantity: existing_cart_item.quantity + quantity)
-      else
-        @cart.cart_items.create(cart_item_params)
-      end
-
-      @cart.update(item_count: @cart.cart_items.sum(:quantity))
-
-      redirect_to user_cart_path(current_user), notice: I18n.t('cart_items.create.success')
     else
       @cart = Cart.find_by(id: session[:cart_id]) || Cart.create
       session[:cart_id] = @cart.id
-
       existing_cart_item = @cart.cart_items.find_by(product_id:)
+    end
 
+    @product = Product.find(product_id)
+
+    if @product.stock_quantity < quantity
+      redirect_back fallback_location: root_path, alert: I18n.t('cart_items.create.insufficient_stock')
+      return
+    end
+
+    ActiveRecord::Base.transaction do
       if existing_cart_item
-        existing_cart_item.update(quantity: existing_cart_item.quantity + quantity)
+        existing_cart_item.update!(quantity: existing_cart_item.quantity + quantity)
       else
-        @cart.cart_items.create(cart_item_params)
+        @cart.cart_items.create!(cart_item_params)
       end
 
-      @cart.update(item_count: @cart.cart_items.sum(:quantity))
+      @product.update!(stock_quantity: @product.stock_quantity - quantity)
 
+      @cart.update!(item_count: @cart.cart_items.sum(:quantity))
+    end
+
+    if current_user
+      redirect_to user_cart_path(current_user), notice: I18n.t('cart_items.create.success')
+    else
       redirect_to root_path, notice: I18n.t('cart_items.create.success')
     end
-  rescue ActiveRecord::RecordNotFound
-    redirect_back fallback_location: root_path, alert: I18n.t('cart_items.create.not_found')
+  rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotFound
+    redirect_back fallback_location: root_path, alert: I18n.t('cart_items.create.error')
   end
 
   def edit
@@ -52,18 +56,26 @@ class CartItemsController < ApplicationController
 
   def destroy
     @cart_item = CartItem.find(params[:id])
+    quantity = @cart_item.quantity
+    product = @cart_item.product
 
-    if current_user
-      @cart = current_user.cart
-      @cart_item.destroy
-      redirect_to user_cart_path(current_user), notice: I18n.t('cart_items.destroy.success')
-    else
-      @cart = Cart.find(session[:cart_id])
-      @cart_item.destroy
-      redirect_to cart_path(@cart), notice: I18n.t('cart_items.destroy.success')
+    ActiveRecord::Base.transaction do
+      @cart_item.destroy!
+
+      product.update!(stock_quantity: product.stock_quantity + quantity)
+
+      if current_user
+        @cart = current_user.cart
+        @cart.update!(item_count: @cart.cart_items.sum(:quantity))
+        redirect_to user_cart_path(current_user), notice: I18n.t('cart_items.destroy.success')
+      else
+        @cart = Cart.find(session[:cart_id])
+        @cart.update!(item_count: @cart.cart_items.sum(:quantity))
+        redirect_to cart_path(@cart), notice: I18n.t('cart_items.destroy.success')
+      end
     end
-  rescue ActiveRecord::RecordNotFound
-    redirect_back fallback_location: root_path, alert: I18n.t('cart_items.destroy.not_found')
+  rescue ActiveRecord::RecordNotFound, ActiveRecord::RecordInvalid
+    redirect_back fallback_location: root_path, alert: I18n.t('cart_items.destroy.error')
   end
 
   private
