9cc94db1d29c797ff73b4cfc5fd3b1c95449e575
Radoan Sharkar <radoan.sharkar@welldev.io>
2024-09-03 10:46:40 +0600

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,9 @@
 class ProductsController < ApplicationController
   load_and_authorize_resource except: %i[index show]
-  before_action :set_product, only: %i[show edit update destroy]
+  before_action :set_product, only: %i[show edit update destroy delete_image]
 
   def index
-    @products = Product.all
+    search
   end
 
   def show
@@ -26,7 +26,13 @@ class ProductsController < ApplicationController
   end
 
   def update
-    if @product.update(product_params)
+    if params[:product][:images].present?
+      params[:product][:images].each do |image|
+        @product.images.attach(image)
+      end
+    end
+
+    if @product.update(product_params.except(:images))
       redirect_to @product, notice: I18n.t('products.update.success')
     else
       render :edit
@@ -38,6 +44,11 @@ class ProductsController < ApplicationController
     redirect_to products_url, notice: I18n.t('products.destroy.success')
   end
 
+  def delete_image
+    @product.images.find(params[:image_id]).purge
+    redirect_to edit_product_path(@product), notice: I18n.t('products.images.delete_success')
+  end
+
   private
 
   def set_product
@@ -46,5 +57,13 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:name, :description, :price, :stock_quantity, :SKU, category_ids: [], images: [])
+  end
+
+  def search
+    @pagy, @products = if params[:q].present?
+                         pagy(Product.where('name ILIKE ?', "%#{params[:q]}%"))
+                       else
+                         pagy(Product.all)
+                       end
   end
 end
