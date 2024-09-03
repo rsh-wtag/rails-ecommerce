8dfbb4dc54fb8e38cecf238d1da80f69ebb204d2
Radoan Sharkar <radoan.sharkar@welldev.io>
2024-09-03 10:43:33 +0600

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,38 +1,99 @@
-class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  after_action :call_custom_action, if: :user_signed_in?
+class CartItemsController < ApplicationController
+  before_action :set_cart_item, only: %i[edit update destroy]
+  before_action :set_cart, only: %i[create]
 
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_path, alert: exception.message
+  def create
+    product_id = params[:cart_item][:product_id]
+    quantity = params[:cart_item][:quantity].to_i
+
+    if current_user
+      @cart = current_user.cart
+      existing_cart_item = @cart.cart_items.find_by(product_id:)
+    else
+      @cart = Cart.find_by(id: session[:cart_id]) || Cart.create
+      session[:cart_id] = @cart.id
+      existing_cart_item = @cart.cart_items.find_by(product_id:)
+    end
+
+    @product = Product.find(product_id)
+
+    if @product.stock_quantity < quantity
+      redirect_back fallback_location: root_path, alert: I18n.t('cart_items.create.insufficient_stock')
+      return
+    end
+
+    ActiveRecord::Base.transaction do
+      if existing_cart_item
+        existing_cart_item.update!(quantity: existing_cart_item.quantity + quantity)
+      else
+        @cart.cart_items.create!(cart_item_params)
+      end
+
+      @cart.update!(item_count: @cart.cart_items.sum(:quantity))
+    end
+
+    if current_user
+      redirect_to user_cart_path(current_user), notice: I18n.t('cart_items.create.success')
+    else
+      redirect_to root_path, notice: I18n.t('cart_items.create.success')
+    end
+  rescue ActiveRecord::RecordInvalid, ActiveRecord::RecordNotFound
+    redirect_back fallback_location: root_path, alert: I18n.t('cart_items.create.error')
   end
 
-  protected
+  def edit
+  end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name address phone role])
+  def update
+    if @cart_item.update(cart_item_params)
+      redirect_to cart_path(@cart_item.cart.user), notice: I18n.t('cart_items.update.success')
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    quantity = @cart_item.quantity
+    product = @cart_item.product
+
+    ActiveRecord::Base.transaction do
+      @cart_item.destroy!
+
+      product.update!(stock_quantity: product.stock_quantity + quantity)
+
+      if current_user
+        @cart = current_user.cart
+        @cart.update!(item_count: @cart.cart_items.sum(:quantity))
+        redirect_to user_cart_path(current_user), notice: I18n.t('cart_items.destroy.success')
+      else
+        @cart = Cart.find(session[:cart_id])
+        @cart.update!(item_count: @cart.cart_items.sum(:quantity))
+        redirect_to cart_path(@cart), notice: I18n.t('cart_items.destroy.success')
+      end
+    end
+  rescue ActiveRecord::RecordNotFound, ActiveRecord::RecordInvalid
+    redirect_back fallback_location: root_path, alert: I18n.t('cart_items.destroy.error')
   end
 
   private
 
-  def call_custom_action
-    nil unless user_signed_in?
-
-    return unless session[:cart_id] && session[:cart_id] != current_user.cart.id
-
-    merge_carts(current_user.cart, Cart.find(session[:cart_id]))
-    session.delete(:cart_id)
+  def set_cart
+    @cart = if current_user
+              current_user.cart
+            else
+              Cart.find(session[:cart_id])
+            end
+  rescue ActiveRecord::RecordNotFound
+    @cart = Cart.create
+    session[:cart_id] = @cart.id
   end
 
-  def merge_carts(user_cart, session_cart)
-    session_cart.cart_items.each do |item|
-      existing_item = user_cart.cart_items.find_by(product_id: item.product_id)
+  def set_cart_item
+    @cart_item = CartItem.find(params[:id])
+  end
 
-      if existing_item
-        existing_item.update(quantity: existing_item.quantity + item.quantity)
-      else
-        user_cart.cart_items.create(product_id: item.product_id, quantity: item.quantity)
-      end
-    end
-    session_cart.destroy
+  def cart_item_params
+    params.require(:cart_item).permit(:product_id, :quantity)
   end
 end
